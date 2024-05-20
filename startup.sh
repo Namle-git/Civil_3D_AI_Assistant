5d42787881bc4ed60cd1216d59fbcb42fd9c6144
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-05-20 16:06:03 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEA
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
 
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+streamlit run Streamlit_app.py --server.port 8000 --server.address 0.0.0.0
