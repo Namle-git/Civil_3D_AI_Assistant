7e9ff2d67c4796201a559a65e4091991233a6fc1
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-05-20 16:01:01 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,10 @@
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get -y install ./google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0
