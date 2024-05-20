84e064c18d596b4de05d12017e3157b9fbb12707
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-05-20 15:47:03 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from urllib.parse import quote
 from bs4 import BeautifulSoup, NavigableString
 import requests
@@ -84,8 +85,15 @@ def get_top_5_links(search_query):
     simulated_search_url = f"https://help.autodesk.com/view/CIV3D/2024/ENU/?query={encoded_query}"
 
     try:
-        # Set up WebDriver (replace with your actual WebDriver path if needed)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # Set up WebDriver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(simulated_search_url)
 
         # Wait for the content to load (adjust the wait time as necessary)
@@ -216,4 +224,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
