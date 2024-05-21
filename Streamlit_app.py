db35cb72a05219a76084d78587dee6706a7ac8a3
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-05-21 10:17:37 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,6 +11,7 @@ import requests
 import google.generativeai as genai
 import os
 
+#Extract_info
 def extract_info(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
