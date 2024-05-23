cb9588925a356cd7632f27ebf25451bf950175df
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-05-23 09:44:43 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,22 +4,53 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from urllib.parse import quote
 from bs4 import BeautifulSoup, NavigableString
 import requests
-import google.generativeai as genai
+from openai import OpenAI
+import os
+
 
 def extract_info(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    original_question = ""
+    """
+        Extracts the original question, top comments, and accepted solutions from a given Civil 3D forum page URL.
 
+        Args:
+            url (str): The URL of the forum page to extract information from.
+
+        Returns:
+            tuple: A tuple containing:
+                - original_question (str): The combined header and body of the original question.
+                - top_comments (list of tuples): A list of the top two comments with their kudos counts.
+                - accepted_solutions (list of str): A list of accepted solutions with their links (if any).
+
+        Raises:
+            requests.RequestException: If there is an issue with the network request.
+    """
+    try:
+        # Send a GET request to the specified URL
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+    except requests.RequestException as e:
+        raise requests.RequestException(f"Failed to retrieve the page: {e}")
+
+    # Parse the content of the response with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Extract the header (subject) of the original question
     header = soup.find('div', class_='lia-message-subject').text.strip()
 
+    # Extract the body content of the original question
     question = soup.find('div', class_='lia-message-body-content').text.strip()
+
+    # Combine the header and the body to form the full original question
     original_question = header + " " + question
-    # Find comments and kudos counts
+
+    # Find all comments on page
     comments = soup.find_all('div', class_='lia-message-body-content')
+
+    # Extract the kudos counts for each comment
     kudos_counts = [int(count.text) for count in
                     soup.find_all('span', class_='MessageKudosCount lia-component-kudos-widget-message-kudos-count')]
 
@@ -52,7 +83,7 @@ def extract_info(url):
                 elif child.name not in ['iframe', 'br']:
                     text_parts.append(child.text.strip())
 
-                    # Insert the link at the position where the iframe was (if it exists)
+            # Insert the link at the position where the iframe was (if it exists)
             if link:
                 # Find the index where the iframe WOULD HAVE BEEN before it was removed
                 iframe_index = -1
@@ -66,29 +97,49 @@ def extract_info(url):
                 else:
                     text_parts.append(f"\n\nLink: {link}\n\n")
 
-                    # Join the text parts to form the final solution text
+            # Join the text parts to form the final solution text
             solution_text = '\n'.join(text_parts)
 
+            # Append the solution text to the list of accepted solutions
             accepted_solutions.append(solution_text)
 
+    # Return the original question, top comments, and accepted solutions
     return original_question, top_comments, accepted_solutions
+
 
 def get_top_5_links(search_query):
     """
-    Simulates searching for a query and retrieving top 5 page links.
-    """
+       Simulates a search for a query on the Autodesk Civil 3D 2024 Help page and retrieves the top 5 pages links.
+
+       Args:
+           search_query (str): The search query string.
+
+       Returns:
+           list: A list of the top 5 links (str) from the search results.
+
+       Raises:
+           Exception: If there is an issue with the web scraping process.
+       """
+    # Encode the search query to be URL-safe
     encoded_query = quote(search_query, safe='')
 
     # Construct the search URL with the specific format (fixed path)
     simulated_search_url = f"https://help.autodesk.com/view/CIV3D/2024/ENU/?query={encoded_query}"
 
     try:
-        # Set up WebDriver (replace with your actual WebDriver path if needed)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # Set up WebDriver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(simulated_search_url)
 
-        # Wait for the content to load (adjust the wait time as necessary)
-        time.sleep(1)  # You might need to adjust this based on page load time
+        # Wait for the content to load
+        time.sleep(1)
 
         # Find the element containing the instructions (inspect the page to get the correct selector)
         links = driver.find_elements(By.TAG_NAME, "a")  # Replace with the actual ID or other selector
@@ -98,34 +149,80 @@ def get_top_5_links(search_query):
         return top_5_links
 
     except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
+
 def extract_text_from_autodesk_help(url):
+    """
+        Extracts text content from a Civil 3D documentation page.
+
+        Args:
+            url (str): The URL of the Autodesk help page.
+
+        Returns:
+            str: The extracted text content from the page.
+
+        Raises:
+            Exception: If there is an issue with the web scraping process.
+        """
     try:
-        # Set up WebDriver (replace with your actual WebDriver path if needed)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # Set up WebDriver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(url)
 
-        # Wait for the content to load (adjust the wait time as necessary)
-        time.sleep(1)  # You might need to adjust this based on page load time
+        # Wait for the content to load
+        time.sleep(1)
 
         # Find the element containing the instructions (inspect the page to get the correct selector)
-        content_element = driver.find_element(By.CLASS_NAME, "caas_body")  # Replace with the actual ID or other selector
+        content_element = driver.find_element(By.CLASS_NAME, "caas_body")
         extracted_text = content_element.text
 
         driver.quit()  # Close the browser
         return extracted_text
 
     except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
+
 def extract_content_from_autodesk_help(url):
+    """
+        Extracts text, image URLs, and video URLs from a Civil 3D documentation page.
+
+        Args:
+            url (str): The URL of the Autodesk help page.
+
+        Returns:
+            tuple: A tuple containing:
+                - extracted_text (list of str): List of text content from the page.
+                - image_urls (list of str): List of image URLs from the page.
+                - video_urls (list of str): List of video URLs from the page.
+
+        Raises:
+            Exception: If there is an issue with the web scraping process.
+        """
     try:
+        # Set up Chrome options for headless mode
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
         # Set up WebDriver
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         driver.get(url)
 
-        time.sleep(1)  # Adjust wait time as needed
+        # Wait for the content to load
+        time.sleep(1)
 
         # Initialize empty lists for different content types
         extracted_text = []
@@ -151,63 +248,120 @@ def extract_content_from_autodesk_help(url):
             if video_url:
                 video_urls.append(video_url)
 
-        driver.quit()
-
+        driver.quit()  # Close the browser
         return extracted_text, image_urls, video_urls
 
     except Exception as e:
+        print(f"An error occurred: {e}")
         return None, None, None  # Return None for all types on error
 
+
 def ask_question_on_autodesk_and_generate_prompt(question):
-    prompt = ""
-    prompt += "Here's some information from 5 different sources. The sources are either the Autodesk Civil 3D documentation or threads from the Civil 3D support forum. Information from the documentation starts with text from article and include links to any images or video in the article. The information from the forum starts with the original question and includes the top 2 most liked responses and the accepted solutions. "
-    prompt += f"Use the information given to answer this question: {question}."
+    """
+       Generates a prompt using information from Autodesk Civil 3D documentation and forum based on a given question.
+
+       Args:
+           question (str): The question to search for.
+
+       Returns:
+           str: The generated prompt with information from documentation and forum.
+       """
+    # Start the prompt with a fixed text
+    prompt = ("Here's some information from 5 different sources. The sources are either the Autodesk Civil 3D "
+              "documentation or threads from the Civil 3D support forum. Information from the documentation starts "
+              "with text from article and include links to any images or video in the article. The information from "
+              "the forum starts with the original question and includes the top 2 most liked responses and the "
+              "accepted solutions. The most relevant text are at the start of the prompt.")
+    # Add the user question to the prompt
+    prompt += f"Use the information given to answer this question: {question}"
+
+    # Get the top 5 links and iterate through them to extract their information then structurally
+    # adding the information into the prompt
     top_5_links = get_top_5_links(search_query=question)
     for link in top_5_links:
+        # Try to extract the page as if it's a forum page
         try:
             original_question, top_comments, accepted_solutions = extract_info(link)
-            prompt += f"Original question: {original_question} \n"
-            prompt += f"Top 2 most liked comments: \n"
+            prompt += f"\n"
+            prompt += f"**Original question**: {original_question} \n"
+            prompt += f"**Top most liked comments**: \n"
             for comment, kudos in top_comments:
                 prompt += comment.text.strip()
-            prompt += f"Accepted solution(s) \n"
+            prompt += f"\n**Accepted solution(s)** \n"
             for solution in accepted_solutions:
                 prompt += solution.strip()
+        # If the page does not contain forum information, extract it as a documentation page
         except:
             text, images, videos = extract_content_from_autodesk_help(link)
             if text:
-                prompt += f"Text from article:\n"
+                prompt += f"\n**Text from article**:\n"
                 for t in text:
                     prompt += t
                     prompt += " "
             if images:
-                prompt += f"Link to images in article:\n"
+                prompt += f"\n**Link to images in article**:\n"
                 for img in images:
                     prompt += img
                     prompt += " "
             if videos:
-                prompt += f"Link to videos in article:\n"
+                prompt += f"\n**Link to videos in article**:\n"
                 for vid in videos:
                     prompt += vid
                     prompt += " "
-    prompt = prompt.replace("\n", " ")
     prompt = prompt.replace("\xa0", " ")
     prompt = prompt.replace("\t", "")
+    prompt = prompt.replace("Solved!\n\nGo to Solution.", "")
+    prompt = prompt.replace("\n\n\n\n", "")
     print(prompt)
     return prompt
 
-def ask_gemini(question):
-    genai.configure(api_key="AIzaSyBlym-KoGLx3x2kMZ_617mWK3i5X5AfTgs")
-    model = genai.GenerativeModel('gemini-1.5-pro-latest')
-    response = model.generate_content(ask_question_on_autodesk_and_generate_prompt(question=question))
+
+def ask_gpt_4o(question):
+    """
+        Sends the prompt to the GPT-4 model and returns the response.
+
+        Args:
+            question (str): The prompt to inject into the GPT-4 model.
+
+        Returns:
+            dict: The response from the GPT-4 model.
+
+        Raises:
+            Exception: If there is an issue with the API request.
+        """
+    # Initiate the OpenAI client
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    # Generate the prompt using the provided question and send the request to the GPT-4o model
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "user",
+                "content": ask_question_on_autodesk_and_generate_prompt(question=question),
+            }
+        ],
+    )
     return response
 
-st.title("Civil 3D helper tool")
 
-user_input = st.text_input("Enter your text:")
-submit_button = st.button("Submit")
+def main():
+    # Initiate a container
+    with st.container():
+        # Display the asistant's name
+        st.title("Civil 3D AI Assistant")
 
-if submit_button:
-    with st.spinner("Processing..."):
-        response = ask_gemini(question=user_input)
-    st.write(response.text)
+        # Get the question from the user
+        user_input = st.text_input("Enter your text:")
+        submit_button = st.button("Submit")
+
+        # Generate the prompt and inject it into GPT 4o
+        if submit_button or user_input:
+            with st.spinner("Processing..."):
+                response = ask_gpt_4o(question=user_input)
+            # Display the generated response
+            st.write(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
