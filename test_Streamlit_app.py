48039071b08de3ffb9f9605cb393de0728ee0278
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-09-26 11:29:11 -0500

    Update test_Streamlit_app.py

--- test_Streamlit_app.py
+++ test_Streamlit_app.py
@@ -2,30 +2,27 @@ import unittest
 from unittest.mock import patch, Mock, MagicMock
 import Streamlit_app
 
-with open("test_forum_page.txt", 'r', encoding='utf-8') as file:
+with open("test_forum_page.html", 'r', encoding='utf-8') as file:
     test_forum_page = file.read()
 
 class TestStreamlitApp(unittest.TestCase):
 
     @patch('Streamlit_app.requests.get')
-    def test_extract_info(self, mock_get):
+    def test_extract_forum_info(self, mock_get):
         # Mocking the response content with HTML example of the forum page
         mock_response = Mock()
         mock_response.content = test_forum_page
         mock_get.return_value = mock_response
 
         url = "http://example.com"
-        original_question, top_comments, accepted_solutions = Streamlit_app.extract_info(url)
+        original_question, accepted_solutions = Streamlit_app.extract_forum_info(url)
         self.assertEqual(original_question,
-                         "Dynamic Block Strech and Rotate Actions Not Cooperating I'm trying to create a block with simple stretch and rotation actions. I have this working almost 100%, but I'd really like to have the rotation grip stay fixed to the tip of the arrow after the base polyline is stretched. I've tried including the rotation action in the action set for the stretch, but then I end up with unexpected results when rotating the block after it's stretched.  Does anyone have ideas on how I could adjust this to get the desired result?\n\n\n\n\t\t\t\t\t\n\t\t\t\t\t\tSolved!\n\t\t\t\t\t\n\t\t\t\t\tGo to Solution.")
-        self.assertEqual(len(top_comments), 2)
-        self.assertEqual(top_comments[0][0].text.strip(),
-                         'You can do this with a more simplistic approach.  Replace the linear parameter and rotation parameter with a Polar Parameter and a Polar Stretch action.  It can to both stretching and rotation in one action.  Include the point/move for the attribute in the polar stretch action.  The point/move action will chain with the stretch and rotation of the polar stretch.\n \n\n \n\nMark Mccall CAD ManglerHammer Land EngineeringLinkedin')
-        self.assertEqual(top_comments[1][0].text.strip(),
-                         "I'd considered that, but we're wanting to be able to specify the rotation angle and the signal head offset distance. Polar parameters will cause it to display as XY coordinate values, right?")
-        self.assertEqual(len(accepted_solutions), 1)
+                         'Curve Table Hii everyone,Is it possible to create curve table like (in img I have attached).If it is possible could anyone please explain the procedure.Thank you\xa0\n\n\n\n\t\t\t\t\t\n\t\t\t\t\t\tSolved!\n\t\t\t\t\t\n\t\t\t\t\tGo to Solution.')
+        self.assertEqual(len(accepted_solutions), 2)
         self.assertEqual(accepted_solutions[0],
-                         '\n\n\nYou can do this with a more simplistic approach.  Replace the linear parameter and rotation parameter with a Polar Parameter and a Polar Stretch action.  It can to both stretching and rotation in one action.  Include the point/move for the attribute in the polar stretch action.  The point/move action will chain with the stretch and rotation of the polar stretch.\n\n\n\n\n\n\n\nMark Mccall CAD ManglerHammer Land EngineeringLinkedin\n')
+                         "I don't know your level of knowledge regarding label design, however you can put just about anything in one label or multiple things from different entities.Here is a video to get your feet wet. This is on annotation labels.https://www.youtube.com/watch?v=2qZ3nC-gL1MI am not sure if there is a way to do it with alignment labels. You can label the alignment with an annotation (think singular) or a alignment label (the entire alignment).https://www.youtube.com/watch?v=AYtIZzloeucHere is a video about alignment labels:Good luck labeling!!")
+        self.assertEqual(accepted_solutions[1],
+                         'I tried editing the label properties and found "vertical speed and station". I wouldn\'t have expected to have seen anything regarding super elevation although you could try some of those other labels within the Cant information. I vaguely remember doing super elevation on one of my projects awhile back, and I don\'t know if that would fall under something that could be referenced with the alignment or not. You might be able to make an expression in the settings tab>alignments>commands>new command, but I don\'t know what.')
 
     @patch('Streamlit_app.webdriver.Chrome')
     def test_get_top_5_links(self, mock_chrome):
@@ -59,36 +56,27 @@ class TestStreamlitApp(unittest.TestCase):
     @patch('Streamlit_app.ChromeDriverManager')
     @patch('Streamlit_app.Options')
     @patch('Streamlit_app.Service')
-    def test_extract_content_from_autodesk_help(self, mock_service, mock_options, mock_chromedriver_manager,
-                                                mock_chrome):
+    def test_extract_content_from_autodesk_help(self, mock_service, mock_options, mock_chromedriver_manager, mock_chrome):
         # Mock the driver
         mock_driver = MagicMock()
         mock_chrome.return_value = mock_driver
 
-        # Mock the content of the page
-        mock_text_element = MagicMock()
-        mock_text_element.text = "Sample text 1\nSample text 2"
-        
-        # Set the return value for find_element (to mock text extraction)
-        mock_driver.find_element.return_value = mock_text_element
+        # Mock page source for BeautifulSoup
+        mock_html = """
+        <div class='caas_body'>
+            <p>Sample text 1</p>
+            <p>Sample text 2</p>
+            <img src='http://example.com/image1.jpg'/>
+            <img src='http://example.com/image2.jpg'/>
+            <video>
+                <source src='http://example.com/video1.mp4'/>
+            </video>
+        </div>
+        """
+        # Set up the mock page source
+        mock_driver.page_source = mock_html
 
-        # Mock image elements
-        mock_image_elements = [MagicMock(), MagicMock(), MagicMock()]
-        mock_image_elements[1].get_attribute.return_value = "http://example.com/image1.jpg"
-        mock_image_elements[2].get_attribute.return_value = "http://example.com/image2.jpg"
-
-        # Mock video elements
-        mock_video_elements = [MagicMock()]
-        mock_video_elements[0].get_attribute.return_value = "http://example.com/video1.mp4"
-
-        # Set the return values for find_elements
-        mock_driver.find_element.return_value = mock_text_element  # For text extraction
-        mock_driver.find_elements.side_effect = [
-            mock_image_elements,  # Second call returns image elements
-            mock_video_elements   # Third call returns video elements
-        ]
-
-        # Mock the URL
+        # Mocking the URL
         url = "http://example.com"
 
         # Call the function
