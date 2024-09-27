f8e1e4d04c8a6add8bfc874b809975f409f86db1
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-09-27 10:43:18 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
-
-# Navigate to the directory where the files are located
-cd /home/site/wwwroot
-
-# Check if the .gz file exists and unzip it
-if [ -f "output.tar.gz" ]; then
-    echo "Extracting output.tar.gz..."
-
-    # Decompress the .gz file
-    gunzip -f output.tar.gz
-
-    echo "Decompression complete."
-fi
-
-# Check if the tar file exists after decompression
-if [ -f "output.tar" ]; then
-    echo "Extracting output.tar..."
-
-    # Extract the tar file
-    tar -xvf output.tar --overwrite
-
-    # Optional: Remove the tar file after extraction
-    rm output.tar
-
-    echo "Extraction complete and output.tar removed."
-else
-    echo "output.tar not found, skipping extraction."
-fi
-
 # Update the package list
 apt-get update
 
