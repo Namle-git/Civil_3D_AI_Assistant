c81c350abfdb915ec037f437c917648a1bc57c0d
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-10-02 22:11:37 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,6 +13,10 @@ import requests
 from openai import OpenAI
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+from azure.ai.contentsafety import ContentSafetyClient
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+from azure.ai.contentsafety.models import AnalyzeTextOptions, TextCategory
 import os
 import logging
 
@@ -385,6 +389,54 @@ def ask_gpt_4o(question, year="2024"):
     )
     return response
 
+def analyze_user_input(input_text):
+    # analyze text
+    key_vault_name = os.environ["KEYVAULT_NAME"]
+    keyVaultRui = f"https://{key_vault_name}.vault.azure.net/"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=keyVaultRui, credential=credential)
+    content_safety_key = client.get_secret("content-safety-key")
+    endpoint = os.environ["CONTENT_SAFETY_ENDPOINT"]
+    input_safe = True
+    # Create an Azure AI Content Safety client
+    client = ContentSafetyClient(endpoint, AzureKeyCredential(content_safety_key))
+
+    # Contruct request
+    request = AnalyzeTextOptions(text=input_text)
+
+    # Analyze text
+    try:
+        response = client.analyze_text(request)
+    except HttpResponseError as e:
+        logging.info("Analyze text failed.")
+        if e.error:
+            logging.info(f"Error code: {e.error.code}")
+            logging.info(f"Error message: {e.error.message}")
+            st.warning("Analyze text failed. The developer has been notified. Please try again later.")
+        else:
+            logging.info(e)
+            st.warning("Analyze text failed. The developer has been notified. Please try again later.")
+    
+    if response:
+        hate_result = next(item for item in response.categories_analysis if item.category == TextCategory.HATE)
+        self_harm_result = next(item for item in response.categories_analysis if item.category == TextCategory.SELF_HARM)
+        sexual_result = next(item for item in response.categories_analysis if item.category == TextCategory.SEXUAL)
+        violence_result = next(item for item in response.categories_analysis if item.category == TextCategory.VIOLENCE)
+
+        if hate_result and hate_result.severity > 1:
+            st.warning("This text contains hate speech.")
+            input_safe = False
+        if self_harm_result and self_harm_result.severity > 1:
+            st.warning("This text contains self-harm content.")
+            input_safe = False
+        if sexual_result and sexual_result.severity > 1:
+            st.warning("This text contains sexual content.")
+            input_safe = False
+        if violence_result and violence_result.severity > 1:
+            st.warning("This text contains violent content.")
+            input_safe = False 
+    return input_safe
+
 
 def main():
     # Initiate a container
@@ -397,15 +449,16 @@ def main():
         )
 
         # Get the question from the user
-        user_input = st.text_input("Please don't enter sensitive information:")
+        user_input = st.text_input("Enter your text:")
         submit_button = st.button("Submit")
 
         # Generate the prompt and inject it into GPT 4o
         if submit_button or user_input:
-            with st.spinner("Processing..."):
-                response = ask_gpt_4o(question=user_input, year=int(year_version))
-            # Display the generated response
-            st.write(response.choices[0].message.content)
+            if analyze_user_input(user_input):
+                with st.spinner("Processing..."):
+                    response = ask_gpt_4o(question=user_input, year=int(year_version))
+                # Display the generated response
+                st.write(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
