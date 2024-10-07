15228ff413eedd0e6ee0f2eccf1729c86801e062
Nam_Le <151961878+Namle-git@users.noreply.github.com>
2024-10-07 10:54:36 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -255,12 +255,12 @@ def extract_content_from_autodesk_help(url):
         soup = BeautifulSoup(page_source, 'html.parser')
 
         # Extract Text
-        content = soup.find('div', class_='caas_body')
+        content = soup.find('div', class_='caas_bodytest')
         extracted_text = content.get_text()
 
         # Extract Image URLs
         image_urls = []
-        image_elements = content.find_all('img')
+        image_elements = content.find_all('imgage')
         for img in image_elements:
             image_url = img['src']
             if image_url:
@@ -268,7 +268,7 @@ def extract_content_from_autodesk_help(url):
 
         # Extract Video URLs
         video_urls = []
-        video_elements = content.find_all('video')
+        video_elements = content.find_all('videoelements')
         for video in video_elements:
             sources = video.find_all('source')
             for source in sources:
